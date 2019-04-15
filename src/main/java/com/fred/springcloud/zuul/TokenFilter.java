cd9db7554d84bb7c379f80cca43a5fb7dc7e91a7
81673211 <81673211@qq.com>
2019-04-15 14:49:10 +0800

    zuul

---
+++ src/main/java/com/fred/springcloud/zuul/TokenFilter.java
@@ -0,0 +1,58 @@
+package com.fred.springcloud.zuul;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ *
+ * <b>Description:.</b><br> 
+ * @author <b>sil.zhou</b>
+ * <br><b>ClassName:</b> 
+ * <br><b>Date:</b> 2019/4/12 15:42
+ */
+@Component
+public class TokenFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return PRE_DECORATION_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext rc = RequestContext.getCurrentContext();
+        HttpServletRequest request = rc.getRequest();
+        System.out.println(String.format("method: %s, url: %s", request.getMethod(), request.getRequestURL()));
+        String token = request.getParameter("token");
+        if (StringUtils.isBlank(token)) {
+            rc.setSendZuulResponse(false);
+            rc.setResponseStatusCode(401);
+            try {
+                rc.getResponse().getWriter().write("token is empty !");
+            } catch (IOException e) {
+
+            }
+        }
+        return null;
+    }
+}
